ac63aeb491f41ddfeac3b60a9b4a954a48668c8a
rahulde <rahulde@thoughtworks.com>
2015-09-27 12:53:29 +0530

    RD |uwsgi| added controllers for uWSGI server

---
+++ app_controllers/nginx_controller.py
@@ -0,0 +1,47 @@
+import subprocess
+import os
+
+nginx_pid_file = ".NGINX_PID"
+
+
+def render_nginx_config(port):
+    print "Rendering config for nginx..."
+    subprocess.call(['node', 'render_conf.js', str(port)])
+    print "Config rendered."
+
+
+def kill_nginx():
+    if os.path.exists(nginx_pid_file):
+        print "Running nginx process found. Killing it..."
+        with open(nginx_pid_file) as pid:
+            subprocess.call(['kill', pid.readlines()[0].strip()])
+        os.remove(nginx_pid_file)
+        print "nginx stopped."
+
+
+def start_nginx(port):
+    render_nginx_config(port)
+
+    print "Starting nginx..."
+    kill_nginx()
+
+    process = subprocess.Popen(
+        ["nginx", "-c", os.getcwd() + "/app/server_configs/nginx.conf"])
+
+    process.wait()
+    if process.poll() != 0:
+        print "nginx start failed."
+    else:
+        with open(nginx_pid_file, "w") as pid:
+            pid.write(str(process.pid + 1))  # TODO: Possibly flaky PID check.
+        print "nginx successfully started on port %d" % port
+
+
+def handle_nginx(mode, port):
+    if mode == "start":
+        start_nginx(port)
+    elif mode == "stop":
+        kill_nginx()
+    else:
+        kill_nginx()
+        start_nginx()
---
+++ app_controllers/uwsgi_controller.py
@@ -0,0 +1,37 @@
+import subprocess
+import os
+
+uwsgi_pid_file = ".UWSGI_PID"
+
+
+def kill_uwsgi():
+    if os.path.exists(uwsgi_pid_file):
+        print "Running uwsgi process found. Killing it..."
+        with open(uwsgi_pid_file) as pid:
+            subprocess.call(['kill', pid.readlines()[0].strip()])
+        os.remove(uwsgi_pid_file)
+        print "uwsgi stopped."
+
+
+def start_uwsgi():
+    print "Starting uwsgi..."
+    kill_uwsgi()
+
+    os.chdir("app")
+    process = subprocess.Popen(
+        ["uwsgi", "--ini", "server_configs/uwsgi.ini", "--enable-threads"])
+    os.chdir("..")
+
+    with open(uwsgi_pid_file, "w") as pid:
+        pid.write(str(process.pid))  # TODO: Possibly flaky PID check.
+    print "uwsgi successfully started."
+
+
+def handle_uwsgi(mode):
+    if mode == "start":
+        start_uwsgi()
+    elif mode == "stop":
+        kill_uwsgi()
+    else:
+        kill_uwsgi()
+        start_uwsgi()
