d0141dc4ee77e7c3b0c5b52a885f0be670510a00
rahulde <rahulde@thoughtworks.com>
2015-09-29 19:39:08 +0530

    RD | testing | Added a testing route

--- app/app_core/main.py
+++ app/app_core/main.py
@@ -1,7 +1,13 @@
 from flask import Flask
+
 application = Flask(__name__)
 
 
 @application.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
+
+
+@application.route("/test")
+def show_test():
+    return "<h1>This is a test Route</h1>"
