a6ccfecfaf3ba8396b12e3c21e65b81a397b5cb2
Admin <admin@New-Mac-Book-Pro-15-Retina.local>
2015-10-01 22:10:12 +0530

    Rim.Boti/Learning/signin

--- app/app_core/main.py
+++ app/app_core/main.py
@@ -6,8 +6,3 @@ application = Flask(__name__)
 @application.route("/")
 def serve_home():
     return render_template("home.html")
-
-
-@application.route("/test")
-def show_test():
-    return "<h1>I think charles is going to break things here?</h1>"
