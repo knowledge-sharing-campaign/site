83c3002536434b48bff9e5cbc5866a6415ca36a5
noorelden <nooreldenabdalla@gmail.com>
2015-10-01 23:24:42 +0530

    Yasha, Noor |#Home page| Adding nagivation links.

--- app/app_core/main.py
+++ app/app_core/main.py
@@ -2,7 +2,23 @@ from flask import Flask, render_template
 
 application = Flask(__name__)
 
-
 @application.route("/")
-def serve_home():
+def home():
     return render_template("home.html")
+
+@application.route("/aboutus")
+def about_us():
+    return render_template("aboutus.html")
+
+
+@application.route("/contactus")
+def contact_us():
+    return render_template("contactus.html")
+
+@application.route("/events")
+def events():
+    return render_template("events.html")
+
+@application.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
